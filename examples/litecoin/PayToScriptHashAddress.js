39a431456ad49c105f2ab59e5618d011ad1a9bdb
ashraful sharif <sharif.ashraful@gmail.com>
2014-12-16 15:39:05 +0600

    PayToScriptHashAddress example for litecoin

--- examples/litecoin/PayToScriptHashAddress.js
+++ examples/litecoin/PayToScriptHashAddress.js
@@ -5,7 +5,7 @@ var Script = bitcore.Script;
 var network = bitcore.networks['ltc'].livenet;
 
 
-var script = '2103b623c446548b6ea9c0bd1d14e59ec0082bda042a9e26c1de32cbbf852b55837aac'; // write down your script here
+var script = 'OP_RETURN 58434c524e4748530000000000000000000000010000000005f5e100'; // write down your script here
 var s = Script.fromHumanReadable(script);
 
 var hash = bitcoreUtil.sha256ripe160(s.getBuffer());
